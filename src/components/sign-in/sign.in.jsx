18beb9948dece2e1909f77c6aaf2127316e8b7bf
Yusuf İpek <yusufipek@protonmail.com>
2021-01-18 19:16:10 +0300

    email password sign in works

--- src/components/sign-in/sign.in.jsx
+++ src/components/sign-in/sign.in.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { signInWithGoogle, auth } from "../../firebase/firebase.utils";
 import CustomButton from "../custom-button/custom.button";
 import FormInput from "../form-input/form.input.component";
 import "./sign.in.scss";
@@ -13,8 +13,17 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: " ", password: " " });
+    } catch (error) {
+      console.log(error);
+    }
 
     this.setState({ email: "", password: "" });
   };
